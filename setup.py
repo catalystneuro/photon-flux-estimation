f75e20894d0f808a53c358db71ae6224c323a1b7
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-01-23 12:00:23 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="photon-flux-estimation",
     version="0.1.0",
-    description="Library to compute estimated photon flux from two-photon imaging data",
-    author="CatalystNeuro",
+    author="Alessandra Trapani",
+    author_email='alessandra.trapani@catalystneuro.com',
+    description="Library to compute estimated photon flux from multiphoton imaging data",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/catalystneuro/photon-flux-estimation',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
